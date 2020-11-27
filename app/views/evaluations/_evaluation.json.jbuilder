82ce91395332cf165eef74d3007dbe881de0332f
Ruiqi <jin.952@osu.edu>
2020-11-27 06:19:02 -0800

    New project base

--- app/views/evaluations/_evaluation.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! evaluation, :id, :score, :comment, :created_at, :updated_at
-json.url evaluation_url(evaluation, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  resources :evaluations
-  resources :teams
-  resources :students
-  get 'home_page/index'
-  root 'home_page#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
