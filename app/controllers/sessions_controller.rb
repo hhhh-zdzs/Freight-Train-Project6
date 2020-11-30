89e28f99ae331459738c3cbe83cb19b3aa5827d9
eitanrothberg <rothberg.10@osu.edu>
2020-11-30 13:35:14 -0500

    Name.# login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-      student = Student.find_by(dotNum: params[:session][:dotNum].downcase)
+      student = Student.find_by(dotNum: params[:session][:dotNum].downcase, lname: params[:session][:lname])
       if student
         log_in student
         redirect_to student
       else
         #how to create an error message here??
-        flash[:error] = "Invalid student osu dot number"
+        flash[:error] = "Invalid student osu dot number #{params[:session][:lname] + '.' + params[:session][:dotNum]}"
         render 'new'
       end
   end
